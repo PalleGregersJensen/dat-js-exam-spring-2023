036f0c8864d17c72bb92b6acf76c1de67ab95912
PalleGregersJensen <palle.gregers.jensen@gmail.com>
2023-06-12 10:48:50 +0200

    Mulige øvelser til environment 3

--- environment-03/script.js
+++ environment-03/script.js
@@ -1,1 +1,22 @@
-"use strict";
+import { events } from "./data.js";
+
+("use strict");
+
+window.addEventListener("load", start);
+
+function start() {
+    console.log(events);
+    sortBydate();
+    showEvents(events);
+}
+
+function showEvents(eventsList) {
+  for (const event of eventsList) {
+    const eventHtml = /*html*/ `<li>Title: ${event.title} - Description: ${event.description} - Date: ${event.date} - </li>`;
+    document.querySelector("#events-list").insertAdjacentHTML("beforeend", eventHtml);
+  }
+}
+
+function sortBydate() {
+    events.sort((a, b) => a.date.localeCompare(b.date));
+}
