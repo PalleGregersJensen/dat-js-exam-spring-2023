c39fc3fc2d463ec82866851978fd4edfdbaab322
PalleGregersJensen <palle.gregers.jensen@gmail.com>
2023-06-12 20:33:49 +0200

    Mulige eksamensøvelser til environment 11

--- environment-11/script.js
+++ environment-11/script.js
@@ -1,1 +1,33 @@
 "use strict";
+
+let posts = [];
+
+window.addEventListener("load", start);
+
+async function start() {
+  console.log("JS kører");
+  posts = await getJsonData();
+    console.log(posts);
+    showPosts(posts);
+}
+
+async function getJsonData() {
+  const response = await fetch("posts.json");
+  console.log(response);
+  const data = await response.json();
+  console.log(data);
+  return data;
+}
+
+function showPosts(postList) {
+  for (const post of postList) {
+    const postHtml =
+      /*html*/
+      `<article>
+                    <img src=${post.image} alt="post.caption" />
+                    <h2>${post.caption}</h2>
+                    <p>Likes: ${post.likes}</p>
+                </article>`;
+      document.querySelector("#posts-list").insertAdjacentHTML("beforeend", postHtml);
+  }
+}
