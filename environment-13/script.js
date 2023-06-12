770f4d620076e7a6847b9a14f7a91915f8aee6c3
PalleGregersJensen <palle.gregers.jensen@gmail.com>
2023-06-12 21:55:20 +0200

    Mulige eksamnsopgaver til environment 13

--- environment-13/script.js
+++ environment-13/script.js
@@ -1,1 +1,33 @@
 "use strict";
+
+let tickets = [];
+
+window.addEventListener("load", start);
+
+async function start() {
+    console.log("JS kører");
+    tickets = await fetchJsonData();
+    console.log(tickets);
+    showTickets(tickets);
+}
+
+async function fetchJsonData() {
+    const response = await fetch("tickets.json");
+    console.log(response);
+    const data = await response.json();
+    console.log(data);
+    return data;
+}
+
+function showTickets(ticketList) {
+  for (const ticket of ticketList) {
+    const ticketHtml =
+      /*html*/
+      `<article>
+                    <h3>${ticket.eventName}</h3>
+                    <p class= 'ticketid'>${ticket.id}</p>
+                    <button>Use</button>
+                </article>`;
+    document.querySelector("#tickets-list").insertAdjacentHTML("beforeend", ticketHtml);
+  }
+}
