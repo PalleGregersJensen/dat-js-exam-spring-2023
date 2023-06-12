18cd48f4515b9ba80d75337c1c3f6500b09f633c
PalleGregersJensen <palle.gregers.jensen@gmail.com>
2023-06-12 12:49:33 +0200

    Mulige opgaver til environment 4

--- environment-04/script.js
+++ environment-04/script.js
@@ -1,1 +1,40 @@
 "use strict";
+
+const events = [];
+
+window.addEventListener("load", start);
+
+function start() {
+  console.log("JS kører");
+  console.log(events);
+  document.querySelector("#create-event-form").addEventListener("submit", addObjectToArray);
+}
+
+function addObjectToArray(event) {
+  event.preventDefault();
+  const form = event.target;
+  const title = form.title.value;
+  const description = form.description.value;
+  const date = form.date.value;
+  const newEvent = createEvent(title, description, date);
+  console.log(newEvent);
+  events.push(newEvent);
+  console.log(events);
+  form.reset();
+  document.querySelector("#events-list").innerHTML = "";
+  events.forEach(showEvents);
+}
+
+function createEvent(title, description, date) {
+  const event = {
+    title: title,
+    description: description,
+    date: date,
+  };
+  return event;
+}
+
+function showEvents(event) {
+  const eventHtml = /*html*/ `<li>${event.date} - ${event.title} - ${event.description}</li>`;
+  document.querySelector("#events-list").insertAdjacentHTML("beforeend", eventHtml);
+}
