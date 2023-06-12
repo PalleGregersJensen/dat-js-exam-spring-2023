53bf6ba139a5d54ab003ec33282570be4f572b88
PalleGregersJensen <palle.gregers.jensen@gmail.com>
2023-06-12 14:37:34 +0200

    Mulige opgaver til environment 6 og 7

--- environment-06/script.js
+++ environment-06/script.js
@@ -1,1 +1,56 @@
 "use strict";
+
+const runners = [
+  { name: "Torben", time: 55.05 },
+  { name: "Niels", time: 56.59 },
+  { name: "Anders", time: 57.46 },
+];
+
+window.addEventListener("load", start);
+
+function start() {
+    console.log(runners);
+    console.log("JS kører");
+    addRunnerObjectToArray("Søren", 50.44)
+}
+
+function addRunnerObjectToArray(name, time) {
+    const newRunner = createRunnerObject(name, time);
+    runners.push(newRunner);
+    sortByTime()
+    console.log(runners);
+    const firstPlace = runners[0];
+    console.log(firstPlace);
+    document.querySelector("#gold-name").textContent = firstPlace.name
+    document.querySelector("#gold-time").textContent = firstPlace.time;
+    const secondPlace = runners[1];
+    console.log(secondPlace);
+    document.querySelector("#silver-name").textContent = secondPlace.name;
+    document.querySelector("#silver-time").textContent = secondPlace.time;
+    const thirdPlace = runners[2];
+    console.log(thirdPlace);
+    document.querySelector("#bronze-name").textContent = thirdPlace.name;
+    document.querySelector("#bronze-time").textContent = thirdPlace.time;
+    const runnerUps = runners.slice(3);
+    console.log(runnerUps);
+    showRunners(runnerUps);
+}
+
+function createRunnerObject(name, time) {
+    const runner = {
+        name: name,
+        time: time,
+    }
+    return runner;
+}
+
+function showRunners(runnerList) {
+  for (const runner of runnerList) {
+    const runnerHtml = /*html*/ `<li>${runner.name} - ${runner.time}</li>`;
+    document.querySelector("#runnerups-list").insertAdjacentHTML("beforeend", runnerHtml);
+  }
+}
+
+function sortByTime() {
+    runners.sort((a, b) => a.time - b.time);
+}
--- environment-07/script.js
+++ environment-07/script.js
@@ -1,1 +1,49 @@
 "use strict";
+
+const rooms = [];
+
+window.addEventListener("load", start);
+
+function start() {
+  console.log("JS kører");
+  document.querySelector("#create-room-form").addEventListener("submit", addRoomToArray);
+}
+
+function addRoomToArray(event) {
+  event.preventDefault();
+  const form = event.target;
+  const name = form.name.value;
+  const width = Number(form.width.value);
+  const length = Number(form.length.value);
+  const newRoom = createRoom(name, width, length);
+  console.log(newRoom);
+  rooms.push(newRoom);
+    console.log(rooms);
+    showRooms(rooms);
+    form.reset();
+}
+
+function createRoom(name, width, length) {
+  const room = {
+    name: name,
+    width: width,
+    length: length,
+    calculatedArea: length * width,
+  };
+  return room;
+}
+
+function showRooms(roomsList) {
+    document.querySelector("#rooms-table").innerHTML = "";
+  for (const room of roomsList) {
+    const roomHtml =
+      /*html*/
+      `<tr>
+                      <td>${room.name}</td>
+                      <td>${room.width}</td>
+                      <td>${room.length}</td>
+                      <td>${room.calculatedArea}</td>
+                    <tr>`;
+    document.querySelector("#rooms-table").insertAdjacentHTML("beforeend", roomHtml);
+  }
+}
